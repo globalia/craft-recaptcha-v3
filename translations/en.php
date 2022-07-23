9cf3bbd0be97a5a3cf77a592ae73a3c7799b4825
Nancy Lussier <nlussier@globalia.ca>
2022-07-22 21:54:05 -0400

    Generate token fix + score treshold management

--- translations/en.php
+++ translations/en.php
@@ -2,4 +2,8 @@
 
 return [
     'recaptcha.error.message' => 'An error occurred.',
+    'score.treshold.instructions' => 'The scores are between 0.0 and 1.0.<br />0.0 indicates abusive traffic, 1.0 indicates fair traffic.',
+    'score.treshold.label' => 'Score Treshold',
+    'secret.key.label' => 'Secret Key',
+    'site.key.label' => 'Site Key'
 ];
--- translations/fr.php
+++ translations/fr.php
@@ -1,5 +1,9 @@
 <?php
 
-    return [
-        'recaptcha.error.message' => 'Une erreur est survenue.',
-    ];
+return [
+    'recaptcha.error.message' => 'Une erreur est survenue.',
+    'score.treshold.instructions' => 'Les scores sont compris entre 0.0 et 1.0.<br />0.0 indique un trafic abusif, 1.0 indique un trafic correct.',
+    'score.treshold.label' => 'Seuil de score',
+    'secret.key.label' => 'Clé secrète',
+    'site.key.label' => 'Clé du site'
+];
